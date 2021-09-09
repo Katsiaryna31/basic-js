63075d49f73bed2c0b4dede03948d7fc082224f9
Katsiaryna31 <katia310790@mail.ru>
2021-09-09 22:51:58 +0300

    add 3 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  console.log(sampleActivity);
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  let activity = parseFloat(sampleActivity);
+  if (activity > 0 && activity < 2021 && MODERN_ACTIVITY > activity) {
+    return Math.abs(Math.ceil(HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY/activity) / 0.693));
+  } else {
+    return false;
+  }
 }
