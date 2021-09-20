d2c386cbecb81b8926157f94d5ba8c17ae965e69
Katsiaryna31 <katia310790@mail.ru>
2021-09-21 01:15:44 +0300

    add tasks

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,47 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  let result = [];
+  for (let i = 0 ; i <matrix.length; i++) {
+    for (let j = 0; j <matrix[i].length; j++) {
+        let num = 0;
+        if (matrix[i-1]!== undefined && matrix[i-1][j] === true) {
+          num ++;
+        }
+        if (matrix[i+1]!== undefined && matrix[i+1][j] === true) {
+          num ++;
+          console.log(num)
+        }
+        if (matrix[i][j-1]!== undefined && matrix[i][j-1] === true) {
+          console.log(num)
+          num ++;
+        }
+        if (matrix[i][j+1] !== undefined && matrix[i][j+1] === true) {
+          num ++;
+        }
+        if (num === 0) {
+          result.push(1);
+        } else {
+          result.push(num);
+        }
+        
+    }
+  }
+  console.log(result);
+  let count2 = 0;
+  result.forEach(el =>{
+    if (el === 2) {
+      count2 ++;
+    }
+  })
+  if (count2 ===0) {
+    result.fill(0);
+  }
+  let subarray = []; //массив в который будет выведен результат.
+  let size = matrix[0].length;
+  for (let i = 0; i <Math.ceil(result.length/size); i++){
+    subarray[i] = result.slice((i*size), (i*size) + size);
+}
+return subarray;
 }
